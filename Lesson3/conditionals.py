b41a8a5c78699d8a83b7937a5a093697580a66d4
Agon-Hashani <agoni-2007@hotmail.com>
2026-02-28 16:49:38 +0100

    Lesseon3

---
+++ Lesson3/conditionals.py
@@ -0,0 +1,18 @@
+from Lesson1.Variables import temperature
+
+age = 18
+
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You cannot vote.")
+
+Temeperatur = 36
+
+if 30 > temperature:
+    print("it's a hot day")
+    temperature <= 30
+elif 20 <= temperature:
+    print("the wether is good")
+else:
+    print("it's cold outside")
